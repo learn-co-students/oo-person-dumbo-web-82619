71262464dfc7952aa502c50dc43251993db7b2c9
Mark DeNardo <mark.denardo@gmail.com>
2019-08-28 16:41:59 -0400

    Marks 2nd pass

--- lib/person.rb
+++ lib/person.rb
@@ -16,12 +16,38 @@ class Person
         # binding.pry
     end
 
+    # def clamp(input, min, max)
+    #     [input, min, max].sort[1]
+    # end
+    def happiness=(range)
+        #nick.happiness = 9
+        #range = 19
+        if range <= 10 && range >=0
+            @happiness = range
+        elsif range > 10 
+            @happiness = 10
+        elsif range < 0
+            @happiness = 0
+        end
+    end
 
-    def get_paid
+    def hygiene=(range)
+        #nick.happiness = 9
+        #range = 19
+        if range <= 10 && range >=0
+            @hygiene = range
+        elsif range > 10 
+            @hygiene = 10
+        elsif range < 0
+            @hygiene = 0
+        end
+
+    end
+
+    def get_paid(salary)
         # when the variable salary is introduced the sum is added to account
-        print "Enter your salary: "
-        salary = gets.chomp
-        @account = salary + 25
+        @bank_account = salary + 25
+        return 'all about the benjamins'
     end
 
     def clean?
@@ -35,7 +61,31 @@ class Person
     end
 
     def take_bath
-    
+        newHygiene = @hygiene+4
+        # binding.pry
+        self.hygiene = (newHygiene)
+        return '♪ Rub-a-dub just relaxing in the tub ♫'
     end
+
+    def work_out
+        new_happiness = @happiness+2
+        self.happiness = new_happiness
+        newHygiene = @hygiene-3
+        self.hygiene = (newHygiene)
+        return  "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(person)
+        friend_happiness = @happiness+3
+        self.happiness = (friend_happiness)
+       person.happiness += 3
+        if person.name == "person.name"
+            "Hi #{person.name}! It's #{self.name}. How are you?"
+        end
+    end
+
     #binding.pry
 end
+
+
+
